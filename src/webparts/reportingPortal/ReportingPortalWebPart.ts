1b5f2bc327d6464699d1954983db1d4c7736d6ff
sharepointcraft <sharepointcraft@gmail.com>
2025-10-09 15:48:31 +0530

    Basic Setup

---
+++ src/webparts/reportingPortal/ReportingPortalWebPart.ts
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import ReportingPortal from './components/ReportingPortal';
+import { IReportingPortalProps } from './components/IReportingPortalProps';
+export interface IReportingPortalWebPartProps {
+  description: string;
+}
+
+export default class ReportingPortalWebPart extends BaseClientSideWebPart<IReportingPortalWebPartProps> {
+  public render(): void {
+    const element: React.ReactElement<IReportingPortalProps> = React.createElement(
+      ReportingPortal
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+}
---
+++ src/webparts/reportingPortal/components/ReportingPortal.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import type { IReportingPortalProps } from './IReportingPortalProps';
+export default class ReportingPortal extends React.Component<IReportingPortalProps> {
+  public render(): React.ReactElement<IReportingPortalProps> {
+    return (
+     <></>
+    );
+  }
+}
